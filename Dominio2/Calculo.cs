c1081f7c50e1427d45ebcc0de19d745c3e0e9b58
Joel Xavier Pinto <joelxavierpinto@outlook.com>
2020-09-10 23:27:41 -0300

    Ajustes

---
+++ Dominio2/Calculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dominio2
+{
+    public class Calculo
+    {
+        public static double Divisao(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+        public static double Soma(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+        public static double Subtracao(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+        public static double Multiplicacao(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
